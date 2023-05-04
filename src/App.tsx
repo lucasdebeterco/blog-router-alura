4736444c7bc2e02cd4ed7a141828a50c456dfb32
Lucas Wamser Rosa Debeterco <lucasdebeterco@gmail.com>
2023-05-03 23:58:35 -0300

    creating first routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
 import './App.css'
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import {Home} from "./Pages/Home/Home.tsx";
+import {About} from "./Pages/About/About.tsx";
 
 function App() {
     return (
-        <>
-        </>
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Home />}/>
+                <Route path="/about" element={<About />}/>
+            </Routes>
+        </BrowserRouter>
     )
 }
 
---
+++ src/Pages/About/About.tsx
@@ -0,0 +1,7 @@
+export function About() {
+    return (
+        <>
+            Página about
+        </>
+    )
+}
